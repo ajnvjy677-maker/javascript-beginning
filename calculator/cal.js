2ba3b25c512db31f7360c3460b8d66a27eb30494
ajnvjy677-maker <ajnvjy677@gmail.com>
2026-09-24 23:56:49 +0530

    Add files via upload

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,32 +1,46 @@
 const num1 = document.getElementById("num1")
 const num2 = document.getElementById("num2")
 const ans = document.getElementById("ans")
+const sign = document.getElementById("sign")
+const an = document.getElementById("an-s")
 
 function add() {
+    sign.innerText = "+";
     let n1 = Number(num1.value)
     let n2 = Number(num2.value)
     let sum = n1 + n2
-    console.log(sum)
-    ans.innerText = "Answer is:" + sum;
+    return sum,n
 }
 function sub() {
+    sign.innerText = "-";
     let n1 = Number(num1.value)
     let n2 = Number(num2.value)
     let sum = n1 - n2
+    let n =2
     console.log(sum)
     ans.innerText = "Answer is:" + sum;
 }
 function multi() {
+    sign.innerText = "*";
     let n1 = Number(num1.value)
     let n2 = Number(num2.value)
     let sum = n1 * n2
+    let n =3
     console.log(sum)
     ans.innerText = "Answer is:" + sum;
 }
 function divi() {
+    sign.innerText = "/";
     let n1 = Number(num1.value)
     let n2 = Number(num2.value)
     let sum = n1 / n2
+    let n =4
     console.log(sum)
     ans.innerText = "Answer is:" + sum;
+}
+function aws(n) {
+    let aws = add()
+    console.log(n)
+    ans.innerText = "Answer is:" + aws;
+    
 }
